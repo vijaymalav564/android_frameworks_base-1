c6ac568dddefef3e50d6a301992603b7a78c38d2
ezio84 <brabus84@gmail.com>
2019-11-18 22:56:47 +0100

    base: Lockscreen blur level: code fixes
    check if blur_radius < 1 before checking if it's <5
    
    use existing statusbar settings observer to avoid too many settings
    provider calls when triggering processArtwork (it's called also on
    wake events when goint to lockscreen)
    
    set the default system setting value to 2500,
    because then we divide it for 100 to get the stock default value
    that was 25 in MediaArtwork class.
    Remember to set android:defaultValue to 2500 for the seekbar in
    the Settings part of this commit.
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Change-Id: Ieb71b835228e985b1b29b7c9698166dd47be7e95
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>
    Signed-off-by: SparXFusion <s2234nadar@gmail.com>
    
    k

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -259,6 +259,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.Executor;
+import android.database.ContentObserver;
+import android.content.ContentResolver;
 
 import javax.inject.Named;
 import javax.inject.Provider;
@@ -4589,6 +4591,39 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     public boolean isDeviceInteractive() {
         return mDeviceInteractive;
+    }
+
+    private SbSettingsObserver mSbSettingsObserver = new SbSettingsObserver(mHandler);
+    private class SbSettingsObserver extends ContentObserver {
+        SbSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_MEDIA_BLUR),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            if (uri.equals(Settings.Secure.getUriFor(
+                    Settings.System.LOCKSCREEN_MEDIA_BLUR))) {
+                setLockScreenMediaBlurLevel();
+            }
+        }
+
+        public void update() {
+            setLockScreenMediaBlurLevel();
+        }
+    }
+
+    private void setLockScreenMediaBlurLevel() {
+        if (mMediaManager != null) {
+            mMediaManager.setLockScreenMediaBlurLevel();
+        }
     }
 
     private final BroadcastReceiver mBannerActionBroadcastReceiver = new BroadcastReceiver() {
